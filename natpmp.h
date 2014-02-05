7cb3163e1ee35de5e0df5f6e1572b052aef03603
root <root@example.com>
2014-02-04 21:11:28 -0800

    change uint32_t type def

--- natpmp.h
+++ natpmp.h
@@ -41,7 +41,7 @@ POSSIBILITY OF SUCH DAMAGE.
 #if !defined(_MSC_VER) || _MSC_VER >= 1600
 #include <stdint.h>
 #else
-typedef unsigned long uint32_t;
+typedef unsigned __int32 uint32_t;
 typedef unsigned short uint16_t;
 #endif
 #define in_addr_t uint32_t
